4223758385e025b2c8a0061010df192aa7a8e3ec
Laserreafricaine <monpotagermaison@gmail.com>
2026-05-15 12:42:48 +0200

    3 correctio

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'budget-maison-mai2026';
+const CACHE_NAME = 'budget-maison-mai2026-v2-saisie';
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
